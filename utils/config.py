f7585745d3086eb9258ab985e68f8e92118e52f9
Asdfghjkkllljd <staricinal49@gmail.com>
2024-09-10 20:52:27 +0300

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -3,7 +3,7 @@ import environs
 env = environs.Env()
 env.read_env("./.env")
 
-api_id = env.int("API_ID")
+api_id = 7env.int("API_ID")
 api_hash = env.str("API_HASH")
 
 db_type = env.str("DATABASE_TYPE")
